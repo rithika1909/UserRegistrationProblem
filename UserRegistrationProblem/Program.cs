d0f805ea3a1ad5fc2d429fca18208b594e258f2e
Rithika L <rithika.l@tvsnext.com>
2023-08-03 06:46:34 +0530

    [Rithika] Add : Implemented

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validate validate = new Validate();
+            Console.WriteLine("Enter the first name to validate:");
+            string FirstName = Console.ReadLine();
+            validate.FirstName(FirstName);
+        }
+    }
+}
