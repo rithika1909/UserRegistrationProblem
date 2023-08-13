20a936a4712cc0882bd60f41ddb2f1257f6801d1
rithika1909 <113461039+rithika1909@users.noreply.github.com>
2023-08-13 22:46:07 +0530

    Update UnitTest1.cs

--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestFirstName()
         {
-            string input = "Rithikaa";
+            string input = "Rithika";
             Validate firstName = new Validate();
             bool result = firstName.FirstName(input);
             Assert.IsTrue(result);
@@ -25,7 +25,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestEmail()
         {
-            string input = "riyareji@gmail.com";
+            string input = "rithika.l1909@gmail.com";
             Validate email = new Validate();
             bool result = email.Email(input);
             Assert.IsTrue(result);
@@ -33,7 +33,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestPhoneNumber()
         {
-            string input = "91 9012345678";
+            string input = "9789286965";
             Validate phonenumber = new Validate();
             bool result = phonenumber.PhoneNumber(input);
             Assert.IsTrue(result);
@@ -41,7 +41,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestCheckCharacter()
         {
-            string input = "riya@1234";
+            string input = "rithika@123";
             Validate character = new Validate();
             bool result = character.CheckCharacter(input);
             Assert.IsTrue(result);
@@ -49,7 +49,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestUpperCase()
         {
-            string input = "Riya1234";
+            string input = "Rithika123";
             Validate uppercase = new Validate();
             bool result = uppercase.UpperCase(input);
             Assert.IsTrue(result);
@@ -57,7 +57,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestNumeric()
         {
-            string input = "Riya1234";
+            string input = "Rithika123";
             Validate numeric = new Validate();
             bool result = numeric.Numeric(input);
             Assert.IsTrue(result);
@@ -65,7 +65,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestSpecialCharacter()
         {
-            string input = "Riya@1234";
+            string input = "Rithika@123";
             Validate special = new Validate();
             bool result = special.SpecialCharacter(input);
             Assert.IsTrue(result);
