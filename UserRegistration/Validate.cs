5f38efa033958561b3adbef390e80d9294c6850e
Rithika L <rithika.l@tvsnext.com>
2023-08-13 23:12:34 +0530

    [Rithika] Add : Implemented

--- UserRegistration/Validate.cs
+++ UserRegistration/Validate.cs
@@ -58,5 +58,12 @@ namespace UserRegistration
         }
         public bool ValidateRegex(string input) => Regex.IsMatch(input, specialCharacter)? true : false;
 
+
+        public bool ValidateFirstNameRegex(string input) => Regex.IsMatch(input, firstName) ? true : false;
+        public bool ValidateLastNameRegex(string input) => Regex.IsMatch(input, lastName) ? true : false;
+        public bool ValidateEmailRegex(string input) => Regex.IsMatch(input, email) ? true : false;
+        public bool ValidatePhoneNumberRegex(string input) => Regex.IsMatch(input, phoneNumber) ? true : false;
+        public bool ValidatePasswordRegex(string input) => Regex.IsMatch(input, specialCharacter) ? true : false;
+
     }
 }
--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestFirstName()
         {
-            string input = "Rithikaa";
+            string input = "Rithika";
             Validate firstName = new Validate();
             bool result = firstName.FirstName(input);
             string value = "";
@@ -43,7 +43,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestEmail()
         {
-            string input = "riyareji@gmail.com";
+            string input = "rithika.l@gmail.com";
             Validate email = new Validate();
             bool result = email.Email(input);
             string value = "";
@@ -60,7 +60,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestPhoneNumber()
         {
-            string input = "91 9012345678";
+            string input = "91 9789286865";
             Validate phonenumber = new Validate();
             bool result = phonenumber.PhoneNumber(input);
             string value = "";
@@ -77,7 +77,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestCheckCharacter()
         {
-            string input = "riya@1234";
+            string input = "rithika@123";
             Validate character = new Validate();
             bool result = character.CheckCharacter(input);
             string value = "";
@@ -94,7 +94,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestUpperCase()
         {
-            string input = "Riya1234";
+            string input = "Rithika123";
             Validate uppercase = new Validate();
             bool result = uppercase.UpperCase(input);
             string value = "";
@@ -111,7 +111,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestNumeric()
         {
-            string input = "Riya1234";
+            string input = "Rithika123";
             Validate numeric = new Validate();
             bool result = numeric.Numeric(input);
             string value = "";
@@ -128,7 +128,7 @@ namespace UserRegistrationTest
         [Test]
         public void TestSpecialCharacter()
         {
-            string input = "Riya@1234";
+            string input = "Rithika@123";
             Validate special = new Validate();
             bool result = special.SpecialCharacter(input);
             string value = "";
@@ -146,14 +146,54 @@ namespace UserRegistrationTest
         [Test]
         public void TestAllEmails()
         {
-            string[] input = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
-                "abc-100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
+            string[] input = { "abc@gmail.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com","abc-100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
             Validate trueEmails = new Validate();
             foreach (var item in input)
             {
                 bool result = trueEmails.Email(item);
                 Assert.IsTrue(result);
             }
+        }
+
+        [Test]
+        public void TestFirstNameRegex()
+        {
+            string input = "Rithika";
+            Validate firstName = new Validate();
+            bool result = firstName.ValidateFirstNameRegex(input);
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void TestLastNameRegex()
+        {
+            string input = "Logachandran";
+            Validate lastName = new Validate();
+            bool result = lastName.ValidateLastNameRegex(input);
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void TestEmailRegex()
+        {
+            string input = "rithika.l@gmail.com";
+            Validate email = new Validate();
+            bool result = email.ValidateEmailRegex(input);
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void TestPhoneNumberRegex()
+        {
+            string input = "91 9789286965";
+            Validate phonenumber = new Validate();
+            bool result = phonenumber.ValidatePhoneNumberRegex(input);
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void TestPasswordRegex()
+        {
+            string input = "Rithika@123";
+            Validate password = new Validate();
+            bool result = password.ValidatePasswordRegex(input);
+            Assert.IsTrue(result);
         }
     }
 }
